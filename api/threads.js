8ab474bc00448ffbd4fc3648be97617588c95c71
Dan Neame <danneame@gmail.com>
2018-06-24 00:08:09 +0100

    Sort getThreads by last_comment_time, desc

--- api/threads.js
+++ api/threads.js
@@ -2,12 +2,16 @@ const db = require('../db')
 
 const getThreads = () => new Promise((resolve, reject) => {
   db.getConnection()
-    .then(() => db.Thread.find().limit(50).exec((err, threads) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(threads)
-    }))
+    .then(() => db.Thread.find()
+      .limit(50)
+      .sort('-last_comment_time')
+      .exec((err, threads) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(threads)
+      })
+    )
 })
 
 exports.getThreads = getThreads
