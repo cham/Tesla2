f1b2cbf5323b15aa30c756908ae8a1a0c18a45c3
Dan Neame <danneame@gmail.com>
2018-06-23 22:11:17 +0100

    POST /v1/auth/login now uses real User accounts as opposed to always granting auth

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const db = require('../db')
+const bcrypt = require('bcrypt')
 
 const verifyToken = token => new Promise((resolve, reject) => {
   jwt.verify(token, process.env.NS_TESLA_JWT_SECRET, (err, tokenData) => {
@@ -18,6 +20,21 @@ const generateToken = payload => new Promise((resolve, reject) => {
   })
 })
 
-exports.login = (username, password) => generateToken({ username })
+exports.login = (username, password) => new Promise((resolve, reject) => {
+  db.getConnection()
+    .then(() => db.User.findOne({ username }, (err, user) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!user) {
+        return reject(new Error('User not found'))
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return reject(new Error('Invalid credentials'))
+      }
+      resolve(generateToken(user.toObject()))
+    }))
+    .catch(reject)
+})
 
 exports.verify = verifyToken
