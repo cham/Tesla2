33a08cefbfb27a1bac084b18a64f0660d4edf8cb
Dan Neame <danneame@gmail.com>
2018-06-24 11:53:06 +0100

    Require nvm before allowing install script to run

--- install.sh
+++ install.sh
@@ -2,4 +2,13 @@
 . ~/.nvm/nvm.sh
 ./stop.sh
 
-nvm install && nvm use && npm install && echo "Install complete"
+if command -v nvm > /dev/null; then
+  nvm install
+  nvm use
+  npm install
+  echo "Install complete"
+  exit 0
+else
+  echo "Please install nvm before running this script"
+  exit 1
+fi
