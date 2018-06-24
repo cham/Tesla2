9f97847de0fe4f8d49a1f72b512ac8dc3037a010
Dan Neame <danneame@gmail.com>
2018-06-24 10:12:01 +0100

    GET /v1/threads/:urlname route, checks auth, validates against getOne, calls threadsApi.getThread passing req.params.urlname

--- routes/threads.js
+++ routes/threads.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const threadsApi = require('../api/threads')
+const threadsValidator = require('./middleware/validators/threads')
 const authMiddleware = require('./middleware/auth')
 
 const router = express.Router()
@@ -9,5 +10,11 @@ router.get('/', authMiddleware.requireAuth, (req, res) => threadsApi.getThreads(
   .then(threads => res.send({ threads }))
   .catch(e => res.status(400).send({ message: e.message }))
 )
+
+router.get('/:urlname', authMiddleware.requireAuth, threadsValidator.getOne, (req, res) => threadsApi.getThread({ urlname: req.params.urlname })
+  .then(thread => res.send(thread))
+  .catch(e => res.status(400).send({ message: e.message }))
+)
+
 
 module.exports = router
